85a8087678efac83fc6de4043a5526db0111ac41
Caminha <gabrielcaminha255@gmail.com>
2022-10-10 02:57:41 -0300

    feat: changed createToken util inner logic

--- app/backend/src/utils/createToken.utils.ts
+++ app/backend/src/utils/createToken.utils.ts
@@ -2,7 +2,7 @@ import * as Jwt from 'jsonwebtoken';
 
 const jwtSecret = process.env.JWT_SECRET || 'jwt_secret';
 
-export default function tokenProcess(data: object): string {
-  const token = Jwt.sign({ data }, jwtSecret, { expiresIn: '7d' });
+export default function createToken(tag: string, data: string): string {
+  const token = Jwt.sign({ [tag]: data }, jwtSecret, { expiresIn: '7d' });
   return token;
 }

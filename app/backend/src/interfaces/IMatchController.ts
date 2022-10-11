5291d0596ce9cd75dbc8d37b601c8732219aac6f
Caminha <gabrielcaminha255@gmail.com>
2022-10-11 09:41:05 -0300

    feat: updated IMatchControl to mplement saveMatch and finishMatch

--- app/backend/src/interfaces/IMatchController.ts
+++ app/backend/src/interfaces/IMatchController.ts
@@ -2,4 +2,8 @@ import { Request, Response } from "express";
 
 export default interface IMatchController {
   getAll(req: Request, res: Response): Promise<Response>;
+
+  saveMatch(req: Request, res: Response): Promise<Response>;
+
+  finishMatch(req: Request, res: Response): Promise<Response>;
 }

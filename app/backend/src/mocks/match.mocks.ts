c224aee7decd5ce0329deb06096cf0f69678f8fe
Caminha <gabrielcaminha255@gmail.com>
2022-10-11 14:12:23 -0300

    feat: added match mocks

---
+++ app/backend/src/mocks/match.mocks.ts
@@ -0,0 +1,39 @@
+export const validMatchArray = [
+  {
+    id: 1,
+    homeTeam: 16,
+    homeTeamGoals: 1,
+    awayTeam: 8,
+    awayTeamGoals: 1,
+    inProgress: false,
+    teamHome: {
+      teamName: "São Paulo",
+    },
+    teamAway: {
+      teamName: "Grêmio",
+    },
+  },
+  {
+    id: 2,
+    homeTeam: 9,
+    homeTeamGoals: 1,
+    awayTeam: 14,
+    awayTeamGoals: 1,
+    inProgress: false,
+    teamHome: {
+      teamName: "Internacional",
+    },
+    teamAway: {
+      teamName: "Santos",
+    },
+  },
+];
+
+export const simpleMatch = {
+  id: 3,
+  homeTeam: 9,
+  homeTeamGoals: 1,
+  awayTeam: 14,
+  awayTeamGoals: 1,
+  inProgress: false,
+}

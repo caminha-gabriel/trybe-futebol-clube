01177d9024ecf85c8fd6e79e9ce1ae78b4e7be07
Caminha <gabrielcaminha255@gmail.com>
2022-10-13 03:42:15 -0300

    feat: created ITeamClassification interface

---
+++ app/backend/src/interfaces/ITeamClassification.ts
@@ -0,0 +1,12 @@
+export default interface ITeamClassification {
+  name: string;
+  totalPoints: number;
+  totalGames: number,
+  totalVictories: number,
+  totalDraws: number,
+  totalLosses: number,
+  goalsFavor: number,
+  goalsOwn: number,
+  goalsBalance: number,
+  efficiency: string;
+}
